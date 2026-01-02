0ddc2a22954d3da9c89d44c3b8e26b5f02cc7a02
zackycuk <aldizacky6@gmail.com>
2026-01-02 14:20:47 +0700

    Ganti logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,11 +33,11 @@ export default function Navbar() {
         <div className="container mx-auto px-4 flex justify-between items-center">
           
           {/* LOGO */}
-          <Link href="/" className="flex items-center gap-2 group">
-            <div className="w-10 h-10 bg-gradient-to-tr from-primary to-blue-600 rounded-xl flex items-center justify-center text-black shadow-[0_0_15px_rgba(0,220,130,0.5)] group-hover:scale-110 transition-transform">
-              <Cpu className="w-6 h-6" />
-            </div>
-            <span className="text-xl font-bold text-white tracking-wide">Codeva<span className="text-primary">.Tech</span></span>
+          {/* LOGO (Minimalis Tanpa Ikon) */}
+          <Link href="/" className="flex items-center group">
+            <span className="text-2xl font-bold text-white tracking-wide transition-colors group-hover:text-primary">
+              CodevaTech
+            </span>
           </Link>
 
           {/* DESKTOP MENU */}
